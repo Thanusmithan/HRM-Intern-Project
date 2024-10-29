a255912eab6f79f4fcfc195b0546ea904c37d737
Chinthana20490 <159599968+Chinthana20490@users.noreply.github.com>
2024-10-29 09:04:42 +0530

    Update Apply.js

--- src/Pages/Leave/Apply.js
+++ src/Pages/Leave/Apply.js
@@ -1,57 +1,121 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, Button, Row, Col } from 'react-bootstrap';
 
 
+const Apply = ({onSubmit}) => {
 
-const Apply = () => {
+    const [data, setData] = useState(
+        { 
+            name:'',
+            department:'',
+            startDate:'',
+            endDate:'',
+            reason:'',
+         }
+    ); 
+    
+    const handleClear = () => {
+        setData({
+            name: '',
+            department: '',
+            startDate: '',
+            endDate: '',
+            reason: ''
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();             
+        onSubmit(data); // Send form data to App component
+        setData({ name: '', department: '', startDate: '', endDate: '', reason: '' }); 
+    }
+  
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;        
+        setData({ ...data, [name]: value });
+    };
+
     return (
 
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg ">
-                {/* <!-- Card Header with Background Color --> */}
+
                 <div className="w-full p-3 bg-blue-500 rounded-t-lg">
                     <h2 className="text-3xl font-bold text-center text-white">Leave Application Form</h2>
                     <p className="text-center text-white ">Please fill in the details to apply for leave</p>
-                </div>
+                </div>                
 
-                {/* <!-- Card Body --> */}
-                <form>
-                    <div className="mb-3">
-                        <label for="email" className="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                        <input type="text" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your name here" required />
-                    </div>
-                    <div className="mb-3">
-                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Department</label>
-                        <select type="text" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                <Form onSubmit={handleSubmit}>
+                    
+                    <Form.Group className="mb-3">
+                        <Form.Label className="text-sm font-medium text-gray-900">Name</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name='name'
+                            value={data.name}
+                            onChange={handleInputChange}
+                            placeholder="Enter your name here"
+                            required
+                        />
+                    </Form.Group>
+
+                    
+                    <Form.Group className="mb-3">
+                        <Form.Label className="text-sm font-medium text-gray-900">Department</Form.Label>
+                        <Form.Control as="select" name='department' value={data.department} onChange={handleInputChange} required>
                             <option value="" disabled selected>-- Please Select --</option>
-                            <option value="IT">Information Technology</option>
-                            <option value="HR">Human Resorce</option>
-                            <option value="FC">Finance</option>
-                            <option value="OP">Operations</option>
-                        </select>
-                    </div>
-                    <div className="mb-3">
-                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start Date</label>
-                        <input type="date" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your name here" required />
-                    </div>
-                    <div className="mb-3">
-                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">End Date</label>
-                        <input type="date" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your name here" required />
-                    </div>
-                    <div className="mb-3">
-                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Reason for Leave</label>
-                        <textarea type="text" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="State the reason for here.." />
-                    </div>
-                    <div className='flex justify-end'>
-                        <button type="" className="px-2 py-2 mb-2 text-sm font-medium text-white bg-red-500 rounded-lg flex-end focus:outline-none hover:bg-red-700 focus:ring-4 focus:ring-green-300 me-2 dark:bg-green-600 dark:hover:bg-red-700 dark:focus:ring-green-800">Cancel</button>
-                        <button type="" className="px-2 py-2 mb-2 text-sm font-medium text-white bg-green-600 rounded-lg focus:outline-none hover:bg-green-700 focus:ring-4 focus:ring-green-300 me-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Apply Leave</button>
-                    </div>
-                </form>
+                            <option value="Information Technology">Information Technology</option>
+                            <option value="Human Resource">Human Resource</option>
+                            <option value="Finance">Finance</option>
+                            <option value="Operations">Operations</option>
+                        </Form.Control>
+                    </Form.Group>
+
+                    
+                    <Form.Group className="mb-3">
+                        <Form.Label className="text-sm font-medium text-gray-900">Start Date</Form.Label>
+                        <Form.Control type="date" name='startDate' value={data.startDate} onChange={handleInputChange} required />
+                    </Form.Group>
+
+                    
+                    <Form.Group className="mb-3">
+                        <Form.Label className="text-sm font-medium text-gray-900">End Date</Form.Label>
+                        <Form.Control type="date" name='endDate' value={data.endDate} onChange={handleInputChange} required />
+                    </Form.Group>
+
+                    
+                    <Form.Group className="mb-3">
+                        <Form.Label className="text-sm font-medium text-gray-900">Reason for Leave</Form.Label>
+                        <Form.Control as="textarea" name='reason' value={data.reason} onChange={handleInputChange} placeholder="State the reason for leave here.." />
+                    </Form.Group>
+
+                    
+                    <Row className="justify-content-end">
+                        <Col xs="auto">
+                            <Button variant="danger" className="me-2" onClick={handleClear} >
+                                Clear
+                            </Button>
+                            <Button type="submit" variant="success">
+                                Apply Leave
+                            </Button>
+                        </Col>
+                    </Row>
+                </Form>
+
+
+
             </div>
-        </div>
+        </div >
+
+
 
 
     )
 }
 
 export default Apply
+
+
+
+
 
